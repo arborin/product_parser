96358153ca37e8168cceb3f9da185ad5612ab5c4
arborin <nika.kobaidze@gmail.com>
2021-11-29 20:20:30 +0400

    [ADD] timeout

--- scra.py
+++ scra.py
@@ -1,31 +1,74 @@
+# SETUP
+# 1. INSTALL selenium  
+# pip install -U selenium
+
+# 2. INSTALL BeautifulSoup 
+# pip install bs4
+
+# 3. INSTALL lxml library 
+# pip install lxml
+
+# 4. DOWNLOAD CHROME DRIVER: 
+# https://chromedriver.storage.googleapis.com/index.html?path=96.0.4664.45/
+# browser = webdriver.Chrome(r"FULL_PATH_OF_WEBDRIVER.EXE !!!")
+
+
+
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import csv
-import sys
-
-# CHANGE THIS URL IF YOU WANT
-base_url = "https://www.breuninger.com/de/herren/bekleidung/"
-
-# PRODUCT LIMIT FOR CRPDUCT
-# SET -1 FOR GET ALL RECORDS
-product_limit = 10
-
-# 
-page = requests.get(base_url)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# GET HOW MANY PAGES ARE IN THIS CATEGORY
-last_page = soup.find_all('span', class_="suchen-pager__size")
-last_page = int(last_page[0].text)
 
 
-# INFO
-print("------------------------------------------------")
-print("Getting Data from: {}".format(base_url))
-print("Total pages: {}".format(last_page))
-print("------------------------------------------------")
-print("Brand, Description, Color")  
-print("================================================")
+# SETTINGS
+url = "https://en.zalando.de/mens-clothing/"
+driver_path = r"C:\Users\admin\Desktop\chromedriver.exe"
+
+product_limit = 100  # SET -1 TO GET ALL PRODUCTS
+total_products = 0  # PRODUCT COUNTER, DON'T CHANGE
+# END SETTINGS
+
+
+chrome_options = Options()
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--log-level=3')
+
+
+
+# METHOD 1
+# SNED GET REQUEST ! not loading page full
+# SET HEADERS FOR SENDING REQUEST AS BROWSER
+# OTHERWISE IT NOT RESPONDING
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+# response = requests.get(url, headers=headers)
+
+
+# METHOD 2 
+# USE urllib 
+# response = urllib.request.urlopen(url)
+# html = response.read().decode('utf-8')
+
+# METHOD 3
+# WORKING METHOD REQUIRE ADDITIONAL INSTALL selenium AND DOWNLOAD chromedriver
+# pip install -U selenium
+# https://chromedriver.chromium.org/home
+
+
+
+# FIND ALL PAGES COUNT
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+response = requests.get(url, headers=headers).content
+soup = BeautifulSoup(response, 'lxml')
+
+# GET PAGES
+page_text = soup.find_all("span", attrs={'aria-current': True})[0].text
+# CONTENT EXAMPLE: Page 1 of 428
+last_page = page_text.split(" ")[-1] # GET LAST ELEMENT 428
+print("=========================================================")
+print(f"URL: {url}")
+print(f"TOTAL PAGES: {last_page}")
+# END ALL PAGES COUNT
 
 
 # CREATE CSV FILE SAME DIRECTORY
@@ -33,50 +76,80 @@ with open('data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Brand", "Description", "Color"])
 
-    total_products = 0 # counter for info
+    # LOOP FOR ALL PAGES
+    for page in range(1, 3):
 
-    # LOOP PAGES FROM 1 TO last_page
-    for curent_page in range(1, last_page+1):
-
+        # CHECK PRODUCT LIMIT
         if total_products == product_limit:
             break;
 
-        url = "{}?page={}".format(base_url, curent_page)
+        # GENERATE URL
+        page_url = "{}?p={}".format(url, page)
 
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        # GET PRODUCT LIST FORM THIS PAGE
-        products = soup.find_all('div', class_='suchen-produktliste__item suchen-produktliste__item--produkt')
-
-        # COLLECT ALL PRODUCT LINKS
-        product_details_link = []
-
-        for pr in products:
-            product_details_link.append(pr.find_all('a')[0]['href'])
+        # INFO
+        print("=========================================================")
+        print(page_url)
         
+        browser = webdriver.Chrome(driver_path, options=chrome_options)
+        browser.set_script_timeout(5)
+        browser.get(page_url)
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        # GETING ALL "article" TAGS 
+        articles = soup.find_all('article')
 
-        # OPEN EVERY LINK ON THIS PAGE
-        for link in product_details_link:
+        # INFO
+        print(f"product on page: {len(articles)}")
+        print("=========================================================")
 
+        page_links = []
+
+        for i, article in enumerate(articles):
+            if "https" in article.a['href']:
+                page_links.append(article.a['href'])
+                # print("{} -- {}".format(i, article.a['href']))
+
+
+        # OPEN PAGE LINKS AND GET DATA FROM IT
+        for page_link in page_links:
+
+            # CHECK PRODUCT LIMIT
             if total_products == product_limit:
                 break;
-
-            url = "https://www.breuninger.com{}".format(link)
-            details = requests.get(url)
-            details_soap = BeautifulSoup(details.content, 'html.parser')
             
-            # BRAND
-            brand = details_soap.find_all("span", {"itemprop": "name"})[0].text.strip()
-            
-            # DESCRIPTION
-            desc = details_soap.find_all("span", class_="bewerten-zusammenfassung__name")[0].text.strip()
+            print(page_link)
+            page_html = requests.get(page_link, headers=headers).content
+            page_soup = BeautifulSoup(page_html, 'lxml')
 
-            # COLOR
-            # example "Farbe: SCHWARZ"
-            color = details_soap.find_all('div', class_="bewerten-farben__aktiv")[0].text.strip()
-            # ['Farbe', ' SCHWARZ']
-            color = color.split(":")[1].strip()
+            wrapper = page_soup.find("x-wrapper-re-1-4")
+            
+            try:
+                brand = wrapper.a.text
+            except:
+                brand = "BRAND NOT FOUND"
+
+            try:
+                desc = wrapper.h1.text
+            except:
+                desc = "DESC NOT FOUND"
+
+            try:
+                search_color = wrapper.find_all("span")
+                color_span = ""
+                for span_num, span in enumerate(search_color):
+                    if span.text=="Colour:":
+                        color_span = span_num + 1 # next span is color
+                        break;
+                    
+                    # print(span.text)
+                if color_span != "":
+                    color = search_color[color_span].text
+                else:
+                    color = "COLOR NOT FOUND"
+
+            except:
+                color = "COLOR NOT FOUND"
+        
             
             # INFO
             print("{},{},{}".format(brand, desc, color))
@@ -85,9 +158,11 @@ with open('data.csv', 'w', newline='') as file:
             writer.writerow([brand, desc, color])
             total_products += 1
 
+
 file.close()
 # INFO
 print("------------------------------------------------")
 print("Total products add: {}".format(total_products))
 print("End")  
 print("------------------------------------------------")
+        
