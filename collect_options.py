873a1d6fee8feb3dea7d28dcf051776854da7898
nkobaidze <nika.kobaidze@gmail.com>
2022-01-19 15:47:03 +0400

    [FIX] get data

--- collect_options.py
+++ collect_options.py
@@ -61,27 +61,131 @@ time.sleep(5)
 # GO TO NEXT MONTH
 country_select = Select(driver.find_element(By.NAME, 'CountryCodeShow'))
 
-print(country_select)
+
 
 # options = [x for x in country_select.find_elements_by_tag_name("option")]
 # this part is cool, because it searches the elements contained inside of select_box 
 # and then adds them to the list options if they have the tag name "options"
+country_data = []
 
-for option in country_select.options:
+# country={'country': "", "code":"", "city":[{"city": "", "code": ""}]}
+
+
+
+
+for option in country_select.options[1:2]:
+
+    country_dict = {}
     country = option.text
     value = option.get_attribute('value')
 
-    print("COUNTRY: {} / CODE: {}".format(country, value))
+    country_dict['country'] = country.strip()
+    country_dict['code'] = value.strip()
+
+    # print("COUNTRY: {} / CODE: {}".format(country, value))
 
     country_select.select_by_value(value)
 
-    time.sleep(2)
+    time.sleep(0.1)
     city_select = Select(driver.find_element(By.NAME, 'PostCodeShow'))
 
-    for city in city_select.options:
-        print(city.text)
+    city_list = []
+    for city_option in city_select.options[1:]:
+        city = {}
+        city['city_name'] = city_option.text.strip()
+        city['code'] = city_option.get_attribute('value').strip()
+
+        city_list.append(city)
     
-    time.sleep(2)
+
+    country_dict['city'] = city_list
+
+        # city_select.select_by_value(value)
+
+        # # CLICK SUBMIT BUTTON
+        # driver.find_element_by_xpath("//input[@type='submit']").click()
+
+        # # CLICK MAKE APPOINTMENT BTN
+        # buttons = driver.find_element_by_tag_name("input").click()
+        # time.sleep(5)
+
+        
+        
+        # GET OPTION CODES AND VALUES
+    country_data.append(country_dict)
+
+
+
+for country in country_data:
+
+    base_url = "https://evisaforms.state.gov/acs/default.asp?postcode="
+
+    for city in country['city']:
+        city_code = city['code']
+
+        city_url = base_url+city_code
+
+        driver.get(city_url)
+
+        # CLICK BUTTON
+        buttons = driver.find_element_by_tag_name("input").click()
+        time.sleep(5)
+
+        # GET OPTIONS
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+
+
+        # GET TITLE AND ADDRESS
+        title_table = soup.find("table")
+        title_trs = title_table.find_all("tr")
+        title_td = title_trs[2].find("td")
+        
+        if title_td:
+            address_line = title_td.text.split('\n')
+            name = address_line[1].strip()
+            address = ("").join(address_line[2:]).strip()
+        
+        print("\n\n\n")
+        print(city['city_name'])
+        print(city_code)
+        print(city_url)
+        print(name)
+        print(address)
+
+
+
+        # GET CALENDAR TABLE
+        option_table = soup.find("table", {'class':'htframe'})
+        # print(option_table)
+        
+        trs = option_table.find_all("tr")
+
+
+
+        for tr in trs:
+            # GET RADIO VALUE
+            radio = tr.find("input")
+            radio_value = ''
+            radio_text = ''
+
+            if radio:
+                radio_value = radio['value']
+            
+            # GET TEXT
+            text_val = tr.find("td",{'class': 'formfield'})
+            if text_val:
+                radio_text = text_val.text
+
+            if radio_value and radio_text:
+                print(f"{radio_value} --- {radio_text}")
+
+            
+            print("---------------------------------------------")
+
+        
+
+
 
     
  
@@ -158,3 +262,6 @@ for option in country_select.options:
 # while True:
 #     schedule.run_pending()
 #     time.sleep(1)
+
+
+
